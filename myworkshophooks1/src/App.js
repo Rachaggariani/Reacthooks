1c29059041d2d3c7d995a0e16d3ca201507eb518
Rachaggariani <rachaghariani506@gmail.com>
2022-08-05 15:08:52 +0100

    first commit

--- myworkshophooks1/src/App.js
+++ myworkshophooks1/src/App.js
@@ -1,40 +1,77 @@
 
-//les bas react hooks
-import { useState } from 'react';
+// //les bas react hooks
+// import { useState } from 'react';
+// import './App.css';
+// function App() {
+//   const [count,setcount]=useState(0);
+//   const [students,setstudents]=useState(["Racha"]);
+//   const [student,setstudent]=useState('');
+//   // console.log(useState(0)[0])
+//   // console.log(useState(0)[1])
+//    const handleIncrement=()=>{
+//     setcount(count+1)
+//   }
+//   const handledecrement=()=>{
+//     if(count>0){setcount(count-1)}
+    
+//   }
+//   const handlechange=(event)=>{
+//     setstudent(event.target.value);
+//   }
+//   const handlesubmit=(e)=>{e.preventDefault();
+// setstudents([...students,student]);
+// setstudent("");
+//   }
+//     return (
+//     <div className="App">
+//     <button onClick={handleIncrement}>+</button>
+//     <h1>{count}</h1>
+//     <button onClick={handledecrement}>-</button>
+//     <form onSubmit={handlesubmit}>
+//       <input type="text" onChange={handlechange} value={student}/>
+//       <button type="submit">submit</button>
+//       {students.map((el,index)=>(<h1 key={index}>{el}</h1>
+//     ))}
+//       </form>
+//     </div>
+//   );
+// }
+// export default App;
+import React, { useState } from 'react'
 import './App.css';
 function App() {
-  const [count,setcount]=useState(0);
-  const [students,setstudents]=useState(["Racha"]);
-  const [student,setstudent]=useState('');
-  // console.log(useState(0)[0])
-  // console.log(useState(0)[1])
-   const handleIncrement=()=>{
-    setcount(count+1)
+  const[users,setusers]=useState([]);
+  const[User,setUser]=useState({email:"",name:""});
+  const handlechangename=(e)=>{
+    setUser({...User, name: e.target.value});
+   
   }
-  const handledecrement=()=>{
-    if(count>0){setcount(count-1)}
-    
+  const handlechangeEmail=(e)=>{
+ setUser({...User, email: e.target.value});
   }
-  const handlechange=(event)=>{
-    setstudent(event.target.value);
+ 
+  const handleSubmit=(event)=>{
+    event.preventDefault();
+    setusers([...users,User]);
+    setUser({email:"",name:""})
   }
-  const handlesubmit=(e)=>{e.preventDefault();
-setstudents([...students,student]);
-setstudent("");
-  }
-    return (
-    <div className="App">
-    <button onClick={handleIncrement}>+</button>
-    <h1>{count}</h1>
-    <button onClick={handledecrement}>-</button>
-    <form onSubmit={handlesubmit}>
-      <input type="text" onChange={handlechange} value={student}/>
-      <button type="submit">submit</button>
-      {students.map((el,index)=>(<h1 key={index}>{el}</h1>
-    ))}
+  return (
+    <div className='App'>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <input type="text" onChange={handlechangename} value={User.name}/>
+          </div>
+        
+          <div>
+          <input type="email" onChange={handlechangeEmail} value={User.email}/>
+          </div>
+        <button type="submit">Submit</button>
       </form>
+      {users.map((el,index)=>(<div key={index}><h1>{el.name}</h1>
+     <h1>{el.email}</h1></div>
+      ))}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
